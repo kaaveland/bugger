417ac4d05541611b88e5f4a1c3f78b6f9ad279bc
Robin Kåveland Hansen <rkh@knowit.no>
2013-06-08 17:30:58 +0200

    Delete trailing whitespace and reindent.

--- db/buggerdb.rb
+++ db/buggerdb.rb
@@ -6,48 +6,48 @@ require 'date'
 require_relative '../config/config'
 
 class BuggerDB
-	def initialize()
-		@db = SQLite3::Database.new(CONFIG['bugger_db'])
-        @db.results_as_hash = true
-	end
+  def initialize()
+    @db = SQLite3::Database.new(CONFIG['bugger_db'])
+    @db.results_as_hash = true
+  end
 
-    def create_empty_db()
-        sql_task = "create table 
+  def create_empty_db()
+    sql_task = "create table
             task (
                 task_id INTEGER PRIMARY KEY,
                 name VARCHAR,
                 description VARCHAR
             );"
-        sql_time_spent = "create table 
+    sql_time_spent = "create table
             task_time (
-                time_id INTEGER PRIMARY KEY, 
-                start INTEGER, 
+                time_id INTEGER PRIMARY KEY,
+                start INTEGER,
                 stop INTEGER,
                 last_update INTEGER,
-                task_id INTEGER, 
+                task_id INTEGER,
                 FOREIGN KEY(task_id) REFERENCES task(task_id)
-            );"        
+            );"
 
-        @db.execute(sql_task)
-        @db.execute(sql_time_spent)
-    end
+    @db.execute(sql_task)
+    @db.execute(sql_time_spent)
+  end
 
-    def initialize_empty_db()
-        sql_task = "insert into task values(null, 'Installing bugger', 'This is how you do it')"
-        @db.execute(sql_task)
-        task_id = @db.last_insert_row_id
-        
-        sql_time_spent = "insert into task_time values(null, strftime('%s','now'), null, strftime('%s','now'), ?)"
-        @db.execute(sql_time_spent, task_id)
-    end
+  def initialize_empty_db()
+    sql_task = "insert into task values(null, 'Installing bugger', 'This is how you do it')"
+    @db.execute(sql_task)
+    task_id = @db.last_insert_row_id
 
-    def execute(sql, parameters)
-        @db.execute(sql, parameters)
-    end
+    sql_time_spent = "insert into task_time values(null, strftime('%s','now'), null, strftime('%s','now'), ?)"
+    @db.execute(sql_time_spent, task_id)
+  end
 
-    def insert(sql, parameters)
-        @db.execute(sql, parameters)
-        @db.last_insert_row_id
-    end
+  def execute(sql, parameters)
+    @db.execute(sql, parameters)
+  end
+
+  def insert(sql, parameters)
+    @db.execute(sql, parameters)
+    @db.last_insert_row_id
+  end
 
 end
